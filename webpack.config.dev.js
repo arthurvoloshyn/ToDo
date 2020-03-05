7adeca1e9be2b8bb03dfbd7c1dcab2c6943a2da3
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-03-05 16:43:16 +0200

    Updated webpack.config.dev.js

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -10,7 +10,7 @@ module.exports = merge(baseConfig, {
   entry: [
     'eventsource-polyfill', // necessary for hot reloading with IE
     'webpack-hot-middleware/client?reload=true', // note that it reloads the page if hot module reloading fails.
-    './src/index'
+    baseConfig.externals.paths.src
   ],
   target: 'web',
   devServer: {
