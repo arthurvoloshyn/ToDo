7880c8b20e97b7a0d85f47cde91c0d4a3d24f442
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-03-05 16:48:55 +0200

    Updated webpack.config.base.js

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,5 +1,6 @@
 import webpack from 'webpack';
 const path = require('path');
+const fs = require('fs');
 
 const PATHS = {
   src: path.join(__dirname, 'src'),
@@ -7,6 +8,9 @@ const PATHS = {
   assets: 'assets/',
   publicPath: '/'
 };
+
+const PAGES_DIR = PATHS.src;
+const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.html')); // eslint-disable-line
 
 const publicUrl = PATHS.publicPath.slice(0, -1);
 
@@ -22,6 +26,18 @@ module.exports = {
     path: PATHS.dist,
     filename: 'bundle.js',
     publicPath: PATHS.publicPath
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          name: 'vendors',
+          test: /node_modules/,
+          chunks: 'all',
+          enforce: true
+        }
+      }
+    }
   },
   plugins: [
     new webpack.DefinePlugin({
