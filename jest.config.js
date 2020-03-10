f3455414f2b87af8f9dd48c91c65f4a0e618a734
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-03-10 16:51:24 +0200

    Updated jest

--- jest.config.js
+++ jest.config.js
@@ -4,9 +4,10 @@ module.exports = {
   transform: {
     '^.+\\.(js|jsx)?$': 'babel-jest'
   },
-  setupTestFrameworkScriptFile: '<rootDir>/testSetup',
+  setupFilesAfterEnv: ['<rootDir>/testSetup'],
   moduleNameMapper: {
     '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/stylesMock.js',
-    '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js'
+    '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js',
+    '~(.*)$': '<rootDir>/src/$1'
   }
 };
